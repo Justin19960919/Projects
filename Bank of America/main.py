9944ebcb21c17e5c6ff6810d6de3409d542dace5
Justin Lee <jclee1996@gmail.com>
2020-10-07 14:08:36 -0700

    New project

---
+++ Bank of America/main.py
@@ -0,0 +1,58 @@
+from download import wait, download, customize_date, pick_date, select_format, download_transaction, sequential_processing, banking, savings, credit_card
+
+from merge import SaveFileInTransaction,FindFile,TransactionsCredit,TransactionsBanking,Payment
+
+import pandas as pd
+import os
+import re
+import numpy as np
+
+from selenium import webdriver
+# headless browser
+# from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup as bs
+from selenium.webdriver.supportui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+
+	## download file start
+	## input account and password
+	print("You have activated the Bank Of America download script")
+	account = input("Please enter your account name: ")
+	password = input("Please enter your password: ")
+	## initiate selenium
+	selection = int(input("Please select which account you want to access:\n1. banking\n2.savings\n3.credit card\n"))
+
+	## user input date
+	start_date = input("Please input start date in format (mm/dd/yyyy): ")
+	end_date = input("Please input start date in format (mm/dd/yyyy): ")
+	date = start_date+"_"+end_date
+	
+	#  User input username
+	username = input("Please type in your username in computer: ")
+
+	if selection == 1:
+		banking(account,password,start_date,end_date)
+		print("During {} to {}; ".format(start_date,end_date))
+		banking_cost = TransactionsBanking(date,username)
+	
+	elif selection == 2:
+		account_name = input("Please type in your account name: (listed in BOA website)")
+		print("During {} to {}; ".format(start_date,end_date))
+		savings(start_date,end_date,account_name)
+	
+	elif selection == 3:
+		credit_card(account,password)
+		print("During {} to {}; ".format(start_date,end_date))
+		credit_card_cost = TransactionsCredit(date,username)
+		## add function to select only transactions in the required timeframe
+
+
+if __name__ == "__main__":
+	main()
+
+
+# default_startdate = "09/03/2020" 
+# default_enddate = "09/15/2020"
---
+++ Bank of America/merge.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import os
+import re
+import numpy as np
+
+#########################################################################################
+
+
+## input pandas dataframe you want to save, and specify which kine of bank account this is
+def SaveFileInTransaction(file,typee,date):
+
+	transactions_dir = "/Users/"+name_in_computer+"/Desktop/boa/Transactions"
+
+	if not os.path.exists(transactions_dir):
+		os.mkdir(transactions_dir)
+	else:
+		os.chdir(transactions_dir)
+		file.to_csv(transactions_dir+"/"+date+"_"+typee+"_"+name_in_computer+".csv",sep=",")
+
+
+## find the downloads file for a certain string eg. "Transactions", then iterate over them to get a pandas dataframe
+def FindFile(findstring,name_in_computer):
+
+	download_route = "/Users/"+name_in_computer+"/Downloads/"
+	os.chdir(download_route)
+	files_in_dir = os.listdir()
+	check_dir = list(map(lambda x: re.search(findstring ,x)!= None,files_in_dir))
+	download_index = np.where(check_dir)[0]
+	#print(download_index)
+
+
+	if len(download_index)>=1:
+
+		myfile = pd.DataFrame()
+		# concatenating all read in files
+		for file in download_index:
+			file_name = files_in_dir[file]
+			#print(file_name)
+			current_read_in_file = pd.read_csv(file_name)
+			os.remove(file_name)  # remove the file from download
+			myfile = pd.concat([myfile,current_read_in_file])
+	
+	else:
+		myfile = "I can't find your Transactions file."
+
+	return myfile
+
+
+
+
+def TransactionsCredit(date,name_in_computer):
+
+	## Searches the downloads file for Transaction.csv files then groups by payee, saved in the boa
+	## transactions file. then removed from the downloads file, returns total money spent on crefit card
+
+	myfile = FindFile("Transaction",name_in_computer)
+	
+	if type(myfile) == "str":
+		print(myfile)
+	else:
+		
+		# strip all amount > 0 
+		myfile = myfile[myfile['Amount']<0]
+		#myfile_groupby_date_and_payee = myfile.groupby(['Posted Date','Payee'])['Amount'].sum()
+		myfile_groupby_payee = myfile.groupby(['Payee'])['Amount'].sum().reset_index()
+
+		#print(myfile_groupby_payee)
+
+		myfile_groupby_payee.columns = ["Payee","Amount"]
+		total_cost = myfile_groupby_payee['Amount'].sum()
+		print(f'Your total expenses in credit card is {total_cost}.')
+		SaveFileInTransaction(myfile_groupby_payee,"Credit",date)
+
+		return total_cost
+
+#TransactionsCredit()
+
+
+
+
+def TransactionsBanking(date,name_in_computer):
+	## takes a statement file from downloads and sums up the money spent,saved in the boa
+	## transactions file. then removed from the downloads file, returns total money spent
+	download_route = "/Users/"+name_in_computer+"/Downloads/"
+
+	with open(download_route+"stmt.csv") as reader:
+
+		reader_list = reader.readlines()[6:] ## summary is from line 0 to 5
+
+		data = []
+		for line in reader_list:
+			
+			line = line.replace("\n","")
+			line = line.replace('"',"")
+			line = line.split(",")
+			if line [0] == "Date":
+				title = line
+			else:
+				data.append(line)
+
+	statement_dict = {}
+
+	for t in range(len(title)):
+
+			statement_dict[title[t]] = list(map(lambda x:x[t] if len(x[t])>0 else "0",data))
+
+	statement_dict = pd.DataFrame(statement_dict)
+	statement_dict['Amount'] = statement_dict['Amount'].astype('float')
+
+	amount_spent = statement_dict['Amount'].sum()
+	running_balance = statement_dict['Running Bal.'][len(statement_dict['Running Bal.'])-1]
+
+	print(f'Your total expenses in your Banking account is ${amount_spent} USD, while your running balance is ${running_balance}')
+
+	SaveFileInTransaction(statement_dict,"BankingAccount",date)
+	os.remove(download_route+"stmt.csv")
+
+	return amount_spent
+
+
+
+#bank = TransactionsBanking()
+#credit = TransactionsCredit()
+
+
+
+## takes two amounts of money
+def Payment(a,b,name_a,name_b):
+
+	a_paid = abs(a)/2
+	b_paid = abs(b)/2
+	payment = a_paid - b_paid
+
+	if payment > 0:
+		msg = "{} needs to give {} {} dollars".format(name_b,name_a,abs(payment))
+	else:
+		msg = "{} needs to give {} {} dollars".format(name_a,name_b,abs(payment))
+
+	return msg
+
